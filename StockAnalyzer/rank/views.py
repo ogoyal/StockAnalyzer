3933e147096502b230fd07df0a2f8f15371cd516
ogoyal <ojasgoyal@gmail.com>
2016-10-19 00:16:40 -0700

    simplify python/stocks.py using iterators

--- StockAnalyzer/rank/views.py
+++ StockAnalyzer/rank/views.py
@@ -2,6 +2,43 @@ from django.shortcuts import render
 from yahoo_finance import Share
 # Create your views here.
 
+class Stock(object):
+    def __init__(self, name):
+        self.name = name
+        self.company = Share(self.name)
+        self.get_price()
+        self.get_date()
+        self.get_delta()
+
+    def get_price(self):
+        self.price = self.company.get_price()
+
+    def get_date(self):
+        full_date = self.company.get_trade_datetime()
+        self.date = full_date.split(' ')[0]
+
+    def get_delta(self):
+        self.delta = self.company.get_change()
+
+def output(slist):
+    f = open('example.csv', 'wt')
+    lines = data(slist)
+    try:
+        writer = csv.writer(f)
+        writer.writerow(('Date', 'Stocks', 'Price($)', 'Change($)'))
+        for i in range(0, len(lines)):
+            writer.writerow(lines[i])
+    finally:
+        f.close()
+
+def data(slist):
+    lines = []
+    for i in range(0,len(slist)):
+        stock = Stock(slist[i])
+        line = [stock.date, stock.name, stock.price, stock.delta]
+	lines.append(line)
+    return lines
+
 def rank_list(request):
     tickers = []
     fopen = open('../tickers.txt', 'r')
@@ -9,5 +46,9 @@ def rank_list(request):
         tickers.append(line.strip())
     fopen.close()
 
+    #company = Stock(tickers)
+
     return render(request, 'template/rank_list.html', {'tickers': tickers})
+
+
 
--- python/stocks.py
+++ python/stocks.py
@@ -10,41 +10,49 @@ import csv
 from yahoo_finance import Share
 
 class Stock(object):
-    def __init__(self, name):
-        self.name = name
-        self.company = Share(self.name)
-        self.get_price()
-        self.get_date()
-        self.get_delta()
+    def __init__(self, slist=[], name=''):
+        self.slist = slist
+        if self.slist != []:
+            self.name = slist[0]
+            self.company = Share(self.name)
+        if name != "":
+            self.name = name
+
+    def __iter__(self):
+        for company in self.slist:
+            scalc = Stock(company)
+            scalc_date = scalc.get_date()
+            scalc_price = scalc.get_price()
+            scalc_delta = scalc.get_delta()
+            yield [scalc_date, company, scalc_price, scalc_delta]
 
     def get_price(self):
         self.price = self.company.get_price()
+        return self.price
 
     def get_date(self):
-        full_date = self.company.get_trade_datetime()
-        self.date = full_date.split(' ')[0]
+        try:
+            full_date = self.company.get_trade_datetime()
+            self.date = full_date.split(' ')[0]
+        except:
+            self.date = "---"
+        return self.date
 
     def get_delta(self):
         self.delta = self.company.get_change()
+        return self.delta
 
 def output(slist):
     f = open('example.csv', 'wt')
-    lines = data(slist)
+    company = Stock(slist)
+    cline = company.__iter__()
     try:
         writer = csv.writer(f)
         writer.writerow(('Date', 'Stocks', 'Price($)', 'Change($)'))
-        for i in range(0, len(lines)):
-            writer.writerow(lines[i])
+        for i in range(len(slist)):
+            writer.writerow(cline.next())
     finally:
         f.close()
-
-def data(slist):
-    lines = []
-    for i in range(0,len(slist)):
-        stock = Stock(slist[i])
-        line = [stock.date, stock.name, stock.price, stock.delta]
-	lines.append(line)
-    return lines
 
 def main():
     tickers = []
@@ -53,6 +61,7 @@ def main():
         tickers.append(line.strip())
     f.close()
     output(tickers)
+ 
 
 if __name__== "__main__":
     main()
