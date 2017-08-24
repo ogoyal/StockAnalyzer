818a3a20daadd069794f30d6e87a25eeefa03d07
ogoyal <ojasgoyal@gmail.com>
2017-08-23 23:56:16 -0700

    diff function

--- python/diffme.sh
+++ python/diffme.sh
@@ -2,10 +2,17 @@
 
 commit_ids=`git log | grep commit | awk '{print $2}' | awk 'length($0)==40'`
 commit_prev=( $commit_ids ) # Get latest commit id
+file=README.md
+
+getdiff() {
+  # Log diff & ignore any diff errors
+  git diff $1:$file $2:$file >> delta.txt 2> /dev/null
+}
 
 for commit in $commit_ids
 do
-  git diff $commit:README.md $commit_prev:README.md >> delta.txt 2> /dev/null # Log diff ; hide error
+  getdiff $commit $commit_prev
   commit_prev=$commit
 done
+
 
