55e853782749e3931c0820b6152b80e2c530bfa4
ogoyal <ojasgoyal@gmail.com>
2017-08-25 01:05:44 -0700

    get dates by commit id

--- python/diffme.sh
+++ python/diffme.sh
@@ -3,17 +3,35 @@
 commit_ids=`git log | grep commit | awk '{print $2}' | awk 'length($0)==40'`
 commit_prev=( $commit_ids ) # Get latest commit id
 file=README.md
+commit_dates=()
 
 getdiff() {
   # Diff between two commit ids
   git diff $1:$file $2:$file
 }
 
+diffdate() {
+  # Diff date based on commit id
+  git show -s --format=%ci $1 | awk '{ print $1 }'
+}
+
+i=0
 for commit in $commit_ids
 do
   # Log diff & ignore any diff errors
   getdiff $commit $commit_prev >> diff.log 2> /dev/null
   commit_prev=$commit
+
+  commit_date=`diffdate $commit`
+  if [[ ${commit_dates[*]} =~ $commit_date ]]
+  then
+    :
+  else
+    commit_dates[i]=$commit_date
+  fi
+
+  i=$((i+1))
 done
 
+echo ${commit_dates[*]}
 
