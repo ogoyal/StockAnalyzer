3845429c83cafcd5bb27400391bb9cbe2639dacd
ogoyal <ojasgoyal@gmail.com>
2017-09-04 17:52:08 -0700

    removing Django for now

--- Django/rank/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-class IndexSignUpForm(forms.Form):
-    email = forms.EmailField(required=True)
-    first_name = forms.CharField(required=False)
-    last_name = forms.CharField(required=False)
-    username = forms.CharField(required=False)
-    password1 = forms.CharField(widget = forms.PasswordInput, label="Password")
-    password2 = forms.CharField(widget = forms.PasswordInput, label="Confirm Password")
